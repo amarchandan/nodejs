3d4531edfdad36c46dbb4f57d526a0dea51c2f9e
Amar Chandan <78189163+amarchandan@users.noreply.github.com>
2026-06-30 00:30:26 +0000

    varible

---
+++ var.js
@@ -0,0 +1,7 @@
+const id = 6788 // constant nvr chnge
+
+var name = "Amar" // mostly use for function scope but issue in both scope 
+
+let mail = "amar@amar.c" // use for block scope 
+
+console.table([id, name, mail])
